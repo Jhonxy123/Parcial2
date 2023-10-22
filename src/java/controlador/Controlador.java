539703af7f89cec8c04a6a79047f2b38b73a3bf5
JhonGonzalez <jhonxy123@gmail.com>
2023-10-22 02:18:16 -0500

    video 3 acabado

--- src/java/controlador/Controlador.java
+++ src/java/controlador/Controlador.java
@@ -4,12 +4,15 @@
  */
 package controlador;
 
+import Modelo.Empleado;
+import Modelo.EmpleadoDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,6 +21,9 @@ import javax.swing.JOptionPane;
  */
 public class Controlador extends HttpServlet {
 
+    Empleado em=new Empleado();
+    EmpleadoDAO edao=new EmpleadoDAO();
+    int ide;
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -26,6 +32,7 @@ public class Controlador extends HttpServlet {
             if(menu.equals("Clientes")){
                 switch(accion){
                     case "Listar":
+                        
                         break;
                     case "Agregar":
                         break;
@@ -56,12 +63,48 @@ public class Controlador extends HttpServlet {
             if(menu.equals("Empleado")){
                  switch(accion){
                     case "Listar":
+                        List lista=edao.listar();
+                        request.setAttribute("empleados",lista);
                         break;
                     case "Agregar":
+                        String dni=request.getParameter("txtDni");
+                        String nom=request.getParameter("txtNombres");
+                        String tel=request.getParameter("txtTel");
+                        String est=request.getParameter("txtEstado");
+                        String user=request.getParameter("txtUser");
+                        em.setDni(dni);
+                        em.setNom(nom);
+                        em.setTel(tel);
+                        em.setEstado(est);
+                        em.setUser(user);
+                        edao.agregar(em);
+                        request.getRequestDispatcher("controlador?menu=Empleado&accion=Listar").forward(request,response);
                         break;
                     case "Editar":
+                        ide=Integer.parseInt(request.getParameter("id"));
+                        Empleado e=edao.listarId(ide);    
+                        request.setAttribute("empleado",e);
+                        request.getRequestDispatcher("Controlador?menu=Empleado&accion=Listar").forward(request,response); 
+                        break;
+                      case "Actualizar":
+                        String dni1=request.getParameter("txtDni");
+                        String nom1=request.getParameter("txtNombres");
+                        String tel1=request.getParameter("txtTel");
+                        String est1=request.getParameter("txtEstado");
+                        String user1=request.getParameter("txtUser");
+                        em.setDni(dni1);
+                        em.setNom(nom1);
+                        em.setTel(tel1);
+                        em.setEstado(est1);
+                        em.setUser(user1);
+                        em.setId(ide);
+                        edao.actualizar(em);
+                        request.getRequestDispatcher("Controlador?menu=Empleado&accion=Listar").forward(request,response); 
                         break;
                     case "Delete":
+                        ide=Integer.parseInt(request.getParameter("id"));
+                        edao.delete(ide);
+                        
                         break;
                     default:
                         throw new AssertionError();
