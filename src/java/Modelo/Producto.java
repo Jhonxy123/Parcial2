89306162130d71b126bc83217478f92a5403d51a
SantiAlfonso <salfonsop@udistrital.edu.co>
2023-10-22 20:54:10 -0500

    Solucionando error

---
+++ src/java/Modelo/Producto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Santiago199
+ */
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    
+    
+    
+}
